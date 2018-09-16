472d9332328a8a308b4d3e6daeee189569f99fd0
David Buhauer <db@idealdev.dk>
2018-09-16 16:51:05 +0200

    Initial Commit

---
+++ WhatsNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatsNow
+//
+//  Created by David Buhauer on 16/09/2018.
+//  Copyright © 2018 Dabdeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
